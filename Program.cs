3f65e85ebdbe9694f5322fe3be7266c6c313e7ff
rachel22kr <haseongyeon796@gmail.com>
2026-05-06 15:36:27 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MemoApp.Data;
+using MemoApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. DB 연결 (PostgreSQL)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. 서비스 등록 (의존성 주입)
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() // 모든 출처 허용
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors();
+
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
